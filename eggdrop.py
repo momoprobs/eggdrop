9088bea9460424e34dcaaebe8bd4ff8e6c396ba1
Mo <momoprobs@gmail.com>
2015-02-17 11:51:34 +0000

    First commit - Hello World for the program

---
+++ eggdrop.py
@@ -0,0 +1,18 @@
+#Egg drop game - meant to help understand the iconic egg drop problem
+#Created by: Mo
+
+#Prototype 1 - Command Line version
+
+import sys
+
+def main():
+  print 'Hello there', sys.argv[1]
+  # Command line args are in sys.argv[1], sys.argv[2] ..
+  # sys.argv[0] is the script name itself and can be ignored
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+  main()
+
+
